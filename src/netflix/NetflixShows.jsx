e5e07b0a2c118ee5497779324a3624d1bc610cd0
zeel <sundayy119@gmail.com>
2025-10-05 12:28:08 +0530

    Watching component and passing dynamic values

--- src/netflix/NetflixShows.jsx
+++ src/netflix/NetflixShows.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NetflixShows = () => {
+  
+  const shows = [
+    {
+      id:1001,
+      name:"Squid Game"
+    },
+    {
+      id:1002,
+      name:"Money Heist"
+    },
+    {
+      id:1003,
+      name:"Stranger Things"
+    },
+    {
+      id:1004,
+      name:"Breaking Bad"
+    }
+  ]
+
   return (
     <div  style={{textAlign:"center"}}>
         <h1>Shows</h1>
+        <ul>
+          {
+            shows.map((show) => {
+              return (
+                <li>
+                  <Link to={`/watch/${show.name}`}>{show.name}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
     </div>
   )
 }
